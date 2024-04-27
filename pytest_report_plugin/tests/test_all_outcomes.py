fc3ec2cda7627ccad0d15d54f2a49166eabbe03b
Shaik Faizan Roshan Ali <roshan.shaik.ml@gmail.com>
2024-04-27 18:50:17 +0530

    Update test_all_outcomes.py

--- pytest_report_plugin/tests/test_all_outcomes.py
+++ pytest_report_plugin/tests/test_all_outcomes.py
@@ -2,12 +2,10 @@ import pytest
 
 def test_passing():
     assert 1 + 1 == 2
-
-
+    
 def test_error():
     with pytest.raises(ZeroDivisionError):
         1 / 0
-
 
 @pytest.mark.skip(reason="Test is skipped for a reason")
 def test_skipped():
@@ -17,7 +15,6 @@ def test_skipped():
 @pytest.mark.xfail(reason="Expected failure")
 def test_expected_failure():
     assert False
-
 
 def test_unexpected_failure():
     assert False
