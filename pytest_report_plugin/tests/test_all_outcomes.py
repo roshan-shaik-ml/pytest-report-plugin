c2ee7d41ea6cdcb8a5df056406b8c77be5f86eaf
Shaik Faizan Roshan Ali <roshan.shaik.ml@gmail.com>
2024-04-27 18:49:05 +0530

    Add files via upload

---
+++ pytest_report_plugin/tests/test_all_outcomes.py
@@ -0,0 +1,23 @@
+import pytest
+
+def test_passing():
+    assert 1 + 1 == 2
+
+
+def test_error():
+    with pytest.raises(ZeroDivisionError):
+        1 / 0
+
+
+@pytest.mark.skip(reason="Test is skipped for a reason")
+def test_skipped():
+    assert False
+
+
+@pytest.mark.xfail(reason="Expected failure")
+def test_expected_failure():
+    assert False
+
+
+def test_unexpected_failure():
+    assert False
