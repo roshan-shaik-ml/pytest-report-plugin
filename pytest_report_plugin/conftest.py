e279e43812185d202ab5b0c521e68b72af2f805d
Shaik Faizan Roshan Ali <roshan.shaik.ml@gmail.com>
2024-04-27 18:50:49 +0530

    Update conftest.py

--- pytest_report_plugin/conftest.py
+++ pytest_report_plugin/conftest.py
@@ -1,28 +1,48 @@
 import pytest
 from pytest_report_plugin.plugin import ReportPlugin
 
+# Allows plugins and conftest files to perform initial configuration.
+def pytest_configure(config):
 
-@pytest.fixture
+    config.pluginmanager.register(ReportPlugin(config))
+
+# Used to register options and settings.
+def pytest_addoption(parser):
+
+    parser.addoption(
+        "--reporting-enabled",
+        action="store_true",
+        help="Enable reporting to the test report service",
+    )
+    parser.addoption(
+        "--reporting-api-url",
+        action="store",
+        default="",
+        help="URL of the test report service API",
+    )
+    parser.addoption(
+        "--reporting-auth-token",
+        action="store",
+        default="",
+        help="Authorization token for accessing the test report service",
+    )
+
+@pytest.fixture(scope="class")
 def report_plugin_config(request):
 
     # Get option values or use defaults
-    reporting_enabled = request.config.getoption("--reporting-enabled", default=False)
-    reporting_api_url = request.config.getoption("--reporting-api-url", default="")
-    reporting_auth_token = request.config.getoption("--reporting-auth-token", default="")
-    
-    # Validate option values if needed
-    
+    reporting_enabled = request.config.option.reporting_enabled
+    reporting_api_url = request.config.option.reporting_api_url
+    reporting_auth_token = request.config.option.reporting_auth_token
+
     return {
         "reporting_enabled": reporting_enabled,
         "reporting_api_url": reporting_api_url,
-        "reporting_auth_token": reporting_auth_token
+        "reporting_auth_token": reporting_auth_token,
     }
 
-def pytest_addoption(parser):
-    parser.addoption("--reporting-enabled", action="store_true", help="Enable reporting to the test report service")
-    parser.addoption("--reporting-api-url", action="store", default="", help="URL of the test report service API")
-    parser.addoption("--reporting-auth-token", action="store", default="", help="Authorization token for accessing the test report service")
-
-@pytest.fixture
+@pytest.fixture(scope="class")
 def report_plugin(report_plugin_config):
-    return ReportPlugin(report_plugin_config)
+
+    plugin = ReportPlugin(report_plugin_config)
+    return plugin
